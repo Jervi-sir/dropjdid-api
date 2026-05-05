eb39d9e0fc08e5f7e55540d3af19cff69ce78e7b
Jervi-sir <gacembekhira@gmail.com>
2026-05-05 02:50:51 +0100

    .

---
+++ app/Http/Controllers/Api/Catalog/CatalogController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api\Catalog;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CatalogController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/Api/Drops/ListController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Api\Drops;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ListController extends Controller
+{
+    //
+}
